34cb3d3b2eef75bd8268cce5cac66f5ccb6a8e01
belovasv <belovasv@gmail.com>
2020-12-09 21:06:50 +0300

    add second test

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -11,7 +11,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static io.qameta.allure.Allure.step;
 
-public class SberTest {
+public class SearchTest {
 
     @BeforeAll
     static void setup (){
@@ -24,25 +24,6 @@ public class SberTest {
         Configuration.browserCapabilities = capabilities;
 
        // Configuration.remote = "https://user1:1234@" + System.getProperty("remote.browser.url") + ":4444/wd/hub/";
-    }
-
-    @Test
-    void searchSber() {
-        step("Открываем сайт сбербанка", (step) -> {
-            open("https://www.sberbank.ru");
-        });
-        step("Кликаем на 'Вклады'", (step) -> {
-            $x("//*[contains(@aria-label, 'Меню  Вклады')]").click();
-        });
-        step("В меню выбираем 'Счета'", (step) -> {
-            $x("//*[@href= '/ru/person/contributions/special_accounts']").click();
-        });
-        step("Кликаем на сберегательный счет'", (step) -> {
-            $x("//a[@href='/ru/person/contributions/deposits/sb_schet']").click();
-        });
-        step("Проверяем что мы на странице 'Сберегательный счёт'", (step) -> {
-            $x("//*[contains(@class, 'page-teaser-dict__header')]").shouldHave(Condition.exactText("Сберегательный счет"));
-        });
     }
 
     @Test
